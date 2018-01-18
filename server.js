085c30bbd5008b84da48d096efb90842646583bc
Auberon Smith <auberon.smith@gmail.com>
2018-01-18 12:03:19 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+var express = require('express');
+var app = express();
+
+
+
+var port = process.env.PORT || 8080;
+
+
+app.set('view engin', 'ejs');
+
+app.use(express.static(heroku-node-62561095-2 + '/public'));
+
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.listen(port, function() {
+  console.log('App is running on http://localhost:' + port);
+});
+
+
+
+
+
 
